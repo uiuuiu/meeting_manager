407627a5c7740c516b975eeff7a5169cef212c2c
javalue2012 <nguyenvanluong11t1@gmail.com>
2015-07-07 14:49:47 +0700

    tao memberlist

---
+++ metting_manager/db/migrate/20150707074001_create_memberlist.rb
@@ -0,0 +1,9 @@
+class CreateMemberlist < ActiveRecord::Migration
+  def change
+    create_table :memberlists do |t|
+      t.integer :user_id
+      t.integer :group_id
+      t.timestamps null: false
+    end
+  end
+end
