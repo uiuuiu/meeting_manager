83108fa9ab1ef396f135292c93157cd92f7a8a84
javalue2012 <nguyenvanluong11t1@gmail.com>
2015-07-07 15:05:32 +0700

    group member

--- metting_manager/db/migrate/20150707074001_create_memberlist.rb
+++ metting_manager/db/migrate/20150707074001_create_memberlist.rb
@@ -2,7 +2,6 @@ class CreateMemberlist < ActiveRecord::Migration
   def change
     create_table :memberlists do |t|
       t.integer :user_id
-      
       t.integer :group_id
       t.timestamps null: false
     end
