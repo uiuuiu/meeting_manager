0e2ebc1081269894880865101705888c30332619
duy_dy <uiuuiu@users.noreply.github.com>
2015-07-08 13:51:41 +0700

    Delete 20150707074001_create_memberlist.rb

--- metting_manager/db/migrate/20150707074001_create_memberlist.rb
+++
@@ -1,9 +0,0 @@
-class CreateMemberlist < ActiveRecord::Migration
-  def change
-    create_table :memberlists do |t|
-      t.integer :user_id
-      t.integer :group_id
-      t.timestamps null: false
-    end
-  end
-end
