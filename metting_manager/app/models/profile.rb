91eae4b6cb2899fcc40a182d55e3021681c8527b
chuhongson-pc <hongsonbk1@gmail.com>
2015-07-08 15:33:16 +0700

    profile

---
+++ metting_manager/app/models/profile.rb
@@ -0,0 +1,8 @@
+class Profile < ActiveRecord::Base
+  attr_accessible :name, :level, :picture, :user_id
+  mount_uploader :picture, PictureUploader
+  validates :name, presence: true
+  validates :level, presence: true, length: { maximum: 540 }
+  validates :picture, presence: true
+  attr_accessor :is_thumbnable
+end
