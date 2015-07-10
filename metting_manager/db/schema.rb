cf01cf8f5632b893b4bb27678cbdc803010df19a
javalue2012 <nguyenvanluong11t1@gmail.com>
2015-07-10 11:01:23 +0700

    group

--- metting_manager/db/schema.rb
+++ metting_manager/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708063632) do
+ActiveRecord::Schema.define(version: 20150710030150) do
 
   create_table "assigments", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
