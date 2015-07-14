eeaa38a3082e237547ca799bf13ffb6cc04d09da
Duy Dy <duydyy@gmail.com>
2015-07-14 09:26:05 +0700

    luu tam

--- metting_manager/app/controllers/time_orders_controller.rb
+++ metting_manager/app/controllers/time_orders_controller.rb
@@ -1,12 +1,14 @@
 class TimeOrdersController < ApplicationController
 	def new
-		list 	= 		Array.new
-		days 	= 		Array.new
-		months 	= 		Array.new
-		years 	= 		Array.new
-		ids 	= 		Array.new
-		times 	= 		Array.new
-		objlist = 		Array.new
+		list 			= 		Array.new
+		days 			= 		Array.new
+		months 			= 		Array.new
+		years 			= 		Array.new
+		ids 			= 		Array.new
+		times 			= 		Array.new
+		objlist 		= 		Array.new
+		timelist 		=		Array.new
+		timeorder_list  = 		Array.new
 		list 	= 		params[:time_order][:time_signed].split(",")
 		list.each do |f|
 			arr = f.split(" ")
@@ -22,6 +24,18 @@ class TimeOrdersController < ApplicationController
 			:ids => arr[4].to_i
 			}
 			objlist.push(obj)
+		end
+		(objlist.size-1).times do
+			min = objlist.min_by{|a| a[:ids]}
+			timelist.push(min)
+			objlist.delete_at(objlist.index(min))
+			if ((min[:ids]+1) == objlist.min_by{|a| a[:ids]}[:ids])
+				min = objlist.min_by{|a| a[:ids]}
+				timelist.push(min)
+			else
+				timeorder_list.push(timelist)
+				timelist.clear
+			end
 		end
 		binding.pry
 	end
