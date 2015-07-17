2281c04652b2dde04a6cb5aaf248b693a1c2dd43
javalue2012 <nguyenvanluong11t1@gmail.com>
2015-07-17 10:18:51 +0700

    search

--- metting_manager/app/controllers/users_controller.rb
+++ metting_manager/app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
     
   end
 
+  def search
+  	@profiles = Profile.where(:name => params[:name]) #lay param chuyen len
+  	@users = Array.new #khoi tao 1 mang @user moi
+  	@profiles.each do |f| #voi moi cac profiles ta day cac user id vao mang @user
+  		@users.push(User.find_by_id(f.user_id))
+  	end
+  	@group = Group.find_by_id(params[:group_id])#giup dieu huong phai lay group_id
+  	redirect_to user_group_path(@group.user_id,@group, :list => @users)#show trang group show
+  end
+
 end
