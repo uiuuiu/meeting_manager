6942f143fa0a7f74319a43bf960044c418674bda
YOUR NAME <YOUR@EMAIL.com>
2015-07-23 17:13:37 +0700

    find profile

--- metting_manager/app/controllers/users_controller.rb
+++ metting_manager/app/controllers/users_controller.rb
@@ -4,11 +4,18 @@ class UsersController < ApplicationController
   end
 
   def search
-  	@profiles = Profile.where(:name => params[:name]) #lay param chuyen len
-  	@users = Array.new #khoi tao 1 mang @user moi
-  	@profiles.each do |f| #voi moi cac profiles ta day cac user id vao mang @user
-  		@users.push(User.find_by_id(f.user_id))
-  	end
+    @users = Array.new 
+  	if params[:find_style]=="name"&&(@profiles = Profile.where(:name => params[:name]))!=nil
+       #lay param chuyen len
+      @profiles.each do |f| #voi moi cac profiles ta day cac user id vao mang @user
+        @users.push(User.find_by_id(f.user_id))
+      end
+    elsif (@group_name = Group.find_by_name(params[:name]))!=nil
+      @member_list = Memberlist.where(:group_id => @group_name.id)
+      @member_list.each do |f| #voi moi cac profiles ta day cac user id vao mang @user
+        @users.push(User.find_by_id(f.user_id))
+      end
+    end
   	@group = Group.find_by_id(params[:group_id])#giup dieu huong phai lay group_id
   	redirect_to user_group_path(@group.user_id,@group, :list => @users)#show trang group show
   end
