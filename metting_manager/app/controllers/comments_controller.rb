86503e9eb7f752edee7e1881bd5ef18660b0f396
YOUR NAME <YOUR@EMAIL.com>
2015-08-08 11:43:57 +0700

    lich comment

--- metting_manager/app/controllers/comments_controller.rb
+++ metting_manager/app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
   def create
     @time_order = TimeOrder.find(params[:time_order_id])
     @comment = Comment.create(:time_sign => Time.now,:user_id => current_user.id,:time_order_id => @time_order.id,:desc => params[:comment][:desc])
-    # binding.pry
     respond_to do |format|
       format.js
     end
